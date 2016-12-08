fc4e4b954e80d9d96502b105b282c25da4b8b32d
“Jack <Jack@Mac-Mini.local>
2016-12-08 20:07:54 +1100

    things

--- the-entire-thing.py
+++ the-entire-thing.py
@@ -1,27 +1,14 @@
-from requests import get
+from urllib.request import urlopen, Request
+from json import loads
 
-start_page_address = input('Address of start page:')
+input_url = input('Starting URL: ')
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+last_slash_character = len(input_url) - input_url.rfind('/')
 
-page = get(start_page_address.split(";")[0]
-, headers=headers)
-search_string = page.text.lower()
+start_url = input_url[:-last_slash_character] + '.json'
 
-search_phrases = ["the ol' reddit","the ol reddit"]
+print('Using URL:', start_url)
 
-print(page.text)
+recieved_page = urlopen(Request(start_url, headers={'User-Agent': 'Mozilla'})).read().decode("utf-8")
 
-for phrase in search_phrases:
-    if phrase in search_string:
-        # this part is confusing because -1 is what find returns when it fails and also the last element of a list
-        result_characters = []
-        result_characters.append(search_string.find(phrase))
-        while result_characters[-1] is not -1:
-            result_characters.append(search_string.find(phrase, result_characters[-1] + 1))
-        del result_characters[-1]
-
-for result_character in result_characters:
-    start_character = search_string.rfind('<p>', 0, result_character) + 3
-    end_character = search_string.find('</p>', result_character)
-    print(search_string[start_character:end_character])
+page_object = loads(recieved_page)
